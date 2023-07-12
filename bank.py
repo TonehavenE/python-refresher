6b4f4d0504bed32d7b945f55c02df50f4cce3c60
Eben Quenneville <student.equenneville@gmail.com>
2023-07-12 10:53:37 -0400

    Catching negative values and adding documentation.

--- bank.py
+++ bank.py
@@ -10,12 +10,29 @@ Create a simulation of a bank account.
     The account should have a method to print the current balance.
 """
 class BankAccount:
-    def __init__(self, name: str, balance: int or float, account_number: int):
+    """
+    A representation of a bank account, with balance, name, and account number.
+    """
+    def __init__(self, name: str, account_number: int,  balance: int or float = 0):
+        """
+        Initialize an account. 
+        Arguments:
+            name: str, the name of the account
+            account_number: the number representing the account id
+            balance: the amount of money stored in the account
+        """
         self.name = name
         self.balance = balance
         self.account_number = account_number
 
     def withdraw(self, quantity: int or float):
+        """
+        Withdraws <quantity: int> from the account. 
+        Throws a ValueError if <quantity> is less than 0, 
+        or greater than the balance of the account.
+        """
+        if quantity < 0:
+            raise ValueError(f"{quantity} is less than 0!")
         if self.balance >= quantity:
             self.balance -= quantity
             print(f"You have successfully withdrawn {quantity} from your account.")
@@ -23,8 +40,17 @@ class BankAccount:
             raise ValueError(f"You don't have enough money in your account to withdraw {quantity}!")
     
     def deposit(self, quantity: int or float):
+        """
+        Deposits <quantity: int> into the account.
+        Raises a ValueError if <quantity> is less than 0.
+        """
+        if quantity < 0:
+            raise ValueError(f"{quantity} is less than 0!")
         self.balance += quantity
-        print(f"You have deposited {quantity} to your account.")
+        print(f"You have deposited {quantity} to your account.") 
 
     def print_balance(self):
+        """
+        Prints the balance of the account.
+        """
         print(f"Your current balance is {self.balance}.")
--- test_bank.py
+++ test_bank.py
@@ -3,7 +3,7 @@ import bank
 
 class TestBank(unittest.TestCase):
     def test_init(self):
-        account = bank.BankAccount("John Doe", 1000.0, 1234)
+        account = bank.BankAccount("John Doe", 1234, 1000.0)
 
         self.assertEqual(account.name, "John Doe")
         self.assertEqual(account.balance, 1000.0)
@@ -15,20 +15,22 @@ class TestBank(unittest.TestCase):
 
 
     def test_withdraw(self):
-        account = bank.BankAccount("Jane Doe", 1000.0, 5678)
+        account = bank.BankAccount("Jane Doe", 5678, 1000.0)
         self.assertRaises(ValueError, account.withdraw, 100000)
+        self.assertRaises(ValueError, account.withdraw, -1)
         account.withdraw(1000)
         self.assertEqual(account.balance, 0)
         self.assertNotEqual(account.balance, -1000)
         self.assertRaises(TypeError, account.withdraw, "foo")
 
     def test_deposit(self):
-        account = bank.BankAccount("Lorem Ipsum", 0, 314159)
+        account = bank.BankAccount("Lorem Ipsum", 314159, 0)
         self.assertEquals(account.balance, 0)
         account.deposit(500.0)
         self.assertNotEquals(account.balance, 0)
         self.assertEquals(account.balance, 500)
         self.assertRaises(TypeError, account.deposit, "bar")
+        self.assertRaises(ValueError, account.deposit, -1)
 
     def test_print_balance(self):
         account = bank.BankAccount("John Doe", 500.00, 9012)
