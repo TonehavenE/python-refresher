f6d82b7e6c05712c8d4b47b5da0f7e4a4eefaf82
Eben Quenneville <student.equenneville@gmail.com>
2023-07-13 10:53:28 -0400

    move gravity and density of water to global vars

--- physics.py
+++ physics.py
@@ -4,6 +4,10 @@ https://blksail-edu.github.io/docs/module/physics
 Eben Quenneville
 7/13/2023
 """
+
+density_water = 1000 # kg/m^3
+gravity = 9.81 # m/s^2, change if you are in space
+
 def calculate_buoyancy(V: float, density_fluid: float) -> float:
     """
     Arguments:
@@ -14,7 +18,6 @@ def calculate_buoyancy(V: float, density_fluid: float) -> float:
     """
     if (V < 0 or density_fluid < 0):
         raise ValueError("Volume or density is negative.")
-    gravity = 9.81 # m/s^2, adjust if you are in space
     buoyancy = density_fluid * V * gravity
     return buoyancy
 
@@ -28,8 +31,6 @@ def will_it_float(V: float, mass: float) -> bool:
     """
     if (V < 0 or mass < 0):
         raise ValueError("Volume or mass is negative.")
-    density_water = 1000 # kg/m^3
-    gravity = 9.81 # m/s^2
     buoyancy_force = calculate_buoyancy(V, density_water)
     gravity_force = mass * gravity
     if buoyancy_force > gravity_force:
@@ -48,7 +49,6 @@ def calculate_pressure(depth: float) -> float:
     """
     if depth < 0:
         raise Exception("Depth is negative. This function assumes depth is positive below the surface.")
-    density_water = 1000 # kg/m^3
-    gravity = 9.81 # m/s^2
+    
     pressure = density_water * gravity * depth
     return pressure
