b09313ec403b5c62e3ce173e5fecc3ad66468d71
Eben Quenneville <student.equenneville@gmail.com>
2023-07-13 10:18:56 -0400

    adding physics and tests

---
+++ physics.py
@@ -0,0 +1,54 @@
+"""
+Implements the functions described in:
+https://blksail-edu.github.io/docs/module/physics
+Eben Quenneville
+7/13/2023
+"""
+def calculate_buoyancy(V: float, density_fluid: float) -> float:
+    """
+    Arguments:
+        V: float, the volume of the object in cubic meters
+        density_fluid: float, the density of the fluid in kg/m^3
+    Returns:
+        float: buoyancy force in Newtons
+    """
+    if (V < 0 or density_fluid < 0):
+        raise ValueError("Volume or density is negative.")
+    gravity = 9.81 # m/s^2, adjust if you are in space
+    buoyancy = density_fluid * V * gravity
+    return buoyancy
+
+def will_it_float(V: float, mass: float) -> bool:
+    """
+    Arguments:
+        V: float, the volume of the object in m^3
+        mass: float, the mass of the object in kg
+    Returns:
+        bool: True if the object will float, False if it will sink
+    """
+    if (V < 0 or mass < 0):
+        raise ValueError("Volume or mass is negative.")
+    density_water = 1000 # kg/m^3
+    gravity = 9.81 # m/s^2
+    buoyancy_force = calculate_buoyancy(V, density_water)
+    gravity_force = mass * gravity
+    if buoyancy_force > gravity_force:
+        return True
+    elif buoyancy_force == gravity_force:
+        raise Exception("There is neutral buoyancy. It will neither float nor sink.")
+    else:
+        return False
+
+def calculate_pressure(depth: float) -> float:
+    """
+    Arguments:
+        depth: float, the depth in meters
+    Returns:
+        float: pressure in Pascals
+    """
+    if depth < 0:
+        raise Exception("Depth is negative. This function assumes depth is positive below the surface.")
+    density_water = 1000 # kg/m^3
+    gravity = 9.81 # m/s^2
+    pressure = density_water * gravity * depth
+    return pressure
---
+++ test_physics.py
@@ -0,0 +1,32 @@
+"""
+Unit Test cases for physics.py
+Eben Quenneville
+7/13/2023
+"""
+import unittest
+import physics
+
+class TestPhysics(unittest.TestCase):
+    def test_calculate_buoyancy(self):
+        self.assertNotEqual(physics.calculate_buoyancy(1, 1000), 0)
+        self.assertEqual(physics.calculate_buoyancy(0.1, 1000), 981)
+        self.assertRaises(TypeError, physics.calculate_buoyancy, "foo", "bar")
+        self.assertRaises(ValueError, physics.calculate_buoyancy, -100, 1000)
+
+
+    def test_will_it_float(self):
+         self.assertEqual(physics.will_it_float(0.1, 50), True)
+         self.assertNotEqual(physics.will_it_float(0.1, 1000), True)
+         self.assertRaises(TypeError, physics.will_it_float, "0.1", "100")
+         self.assertRaises(ValueError, physics.will_it_float, -0.1, 100)
+         self.assertRaises(Exception, physics.will_it_float, 0.1, 100)
+
+
+    def test_calculate_pressure(self):
+        self.assertEqual(physics.calculate_pressure(10), 98100)
+        self.assertNotEqual(physics.calculate_pressure(100), 0)
+        self.assertRaises(TypeError, physics.calculate_pressure, "foo")
+        self.assertRaises(Exception, physics.calculate_pressure, -100)
+
+if __name__ == "__main__":
+    unittest.main()
